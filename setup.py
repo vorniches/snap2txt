fd4325769c2d94fc17ecfe9049515dc29ec44a05
Sérgio Vorniches <vorniches@gmail.com>
2025-01-23 14:13:57 +0000

    lists handling updated

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
-# setup.py
 from setuptools import setup, find_packages
 
 setup(
     name='snap2txt',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
             'snap2txt = snap2txt.__main__:main'
         ]
     },
-    # Add additional information like author, description, requirements, etc.
-    author='Sérgio Vorniches',
+    include_package_data=True,
+    package_data={
+        'snap2txt': ['.il', '.wl'],
+    },
+    author='Sergei Vorniches',
     author_email='vorniches@gmail.com',
     description='A tool to save project structure and contents to a text file',
     long_description=open('README.md').read(),
@@ -19,12 +21,8 @@ setup(
     url='https://github.com/vorniches/snap2txt',
     license='MIT',
     install_requires=[
-        # List any Python package dependencies here
-        # e.g., 'requests>=2.25.1'
     ],
     classifiers=[
-        # Trove classifiers
-        # Full list at https://pypi.org/classifiers/
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
--- snap2txt/saver.py
+++ snap2txt/saver.py
@@ -5,9 +5,6 @@ import argparse
 def read_list_file(file_path):
     """
     Read a list file (.il or .wl) and return the list of patterns.
-
-    :param file_path: Path to the list file.
-    :return: A list of patterns.
     """
     try:
         with open(file_path, 'r') as file:
@@ -19,12 +16,8 @@ def read_list_file(file_path):
 def match_pattern(path, patterns):
     """
     Check if a given path matches any of the patterns in the list.
-
-    :param path: The file or directory path.
-    :param patterns: A list of patterns to match against.
-    :return: True if path matches any pattern, False otherwise.
     """
-    for pattern in patterns:
+    for pattern in patterns or []:
         if pattern in path:
             return True
     return False
@@ -33,19 +26,22 @@ def save_project_structure_and_files(root_path, output_file, ignore_list=None, w
     """
     Save the project structure and contents of all files in the project to a text file,
     considering ignore and whitelist.
-
-    :param root_path: Root directory of the project.
-    :param output_file: Path to the output text file.
-    :param ignore_list: List of patterns to ignore.
-    :param whitelist: List of patterns to include.
     """
     project_structure = []
     file_contents = []
 
     for root, dirs, files in os.walk(root_path):
         # Filter directories and files based on ignore_list and whitelist
-        dirs[:] = [d for d in dirs if not match_pattern(d, ignore_list or []) and (not whitelist or match_pattern(d, whitelist))]
-        files = [f for f in files if not match_pattern(f, ignore_list or []) and (not whitelist or match_pattern(f, whitelist))]
+        dirs[:] = [
+            d for d in dirs
+            if not match_pattern(d, ignore_list) and
+               (not whitelist or match_pattern(d, whitelist))
+        ]
+        files = [
+            f for f in files
+            if not match_pattern(f, ignore_list) and
+               (not whitelist or match_pattern(f, whitelist))
+        ]
 
         for file in files:
             rel_dir = os.path.relpath(root, root_path)
@@ -66,19 +62,23 @@ def save_project_structure_and_files(root_path, output_file, ignore_list=None, w
         f.write("\n".join(file_contents))
 
 def main():
-    script_dir = os.path.dirname(__file__)  # Get the directory where the script is located
-    il_file = os.path.join(script_dir, '.il')  # Path to .il file
-    wl_file = os.path.join(script_dir, '.wl')  # Path to .wl file
+    script_dir = os.path.dirname(__file__)
+    il_file = os.path.join(script_dir, '.il')
+    wl_file = os.path.join(script_dir, '.wl')
 
     parser = argparse.ArgumentParser(description="Save project structure and file contents.")
     parser.add_argument("--il", help="Use ignore list (.il file)", action="store_true")
     parser.add_argument("--wl", help="Use whitelist (.wl file)", action="store_true")
+    parser.add_argument("--show-locations", help="Show the location of the .il and .wl files", action="store_true")
+
     args = parser.parse_args()
+
+    if args.show_locations:
+        print("IL file is located at:", il_file)
+        print("WL file is located at:", wl_file)
+        sys.exit(0)
 
     ignore_list = read_list_file(il_file) if args.il else None
     whitelist = read_list_file(wl_file) if args.wl else None
 
     save_project_structure_and_files('.', 'project_contents.txt', ignore_list, whitelist)
-
-if __name__ == "__main__":
-    main()
